0a600dc39327cee0a242e7755956507afce1eed9
Ashish Shakya <ashishakya@outlook.com>
2025-05-29 21:11:53 +0545

    remote state

---
+++ gauri/remote_state/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" { // backend is something where you store your state remotely
+    bucket = "ashishakya-terraform-state"
+    key = "dev/terraform.tfstate" // path for the terraform.tfstate file
+    region = "eu-west-1"
+  }
+}
+provider "aws" {
+  region  = "eu-west-1"
+  profile = "proshore-terraform"
+}
+
+resource "aws_instance" "aws_ec2" {
+  ami           = "ami-03d8b47244d950bbb"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "my-instance"
+  }
+}
